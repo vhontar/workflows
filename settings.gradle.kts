046497804ab3af7f5cadc634cc98d23fc656c4e5
vlh-ioki <vladyslav.hontar@ioki.com>
2023-10-04 15:23:10 +0200

    dependencyResolutionManagement was removed unintentally

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,5 +8,13 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "Workflows"
 include(":app")
